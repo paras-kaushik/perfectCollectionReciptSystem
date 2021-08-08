291757ce904b532a28f6f0591581258d7e33ca52
Paras Kaushik <paraskaushik12@gmail.com>
2021-08-08 15:04:12 +0530

    om shree ganesh

---
+++ pdf.js
@@ -0,0 +1,161 @@
+window.onload = function () {
+  var currentdate = new Date();
+  var datetime =
+    currentdate.getDate() +
+    "/" +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    currentdate.getFullYear() +
+    " @ " +
+    currentdate.getHours() +
+    ":" +
+    currentdate.getMinutes() +
+    ":" +
+    currentdate.getSeconds();
+
+  document.getElementById("page-date").innerText = datetime;
+
+  document.getElementById("download").addEventListener("click", () => {
+    var deleteButtons = document.getElementsByClassName("item-delete");
+    for (var i = 0; i < deleteButtons.length; i++) {
+      deleteButtons[i].style = "display: none";
+    }
+
+    const invoice = this.document.getElementById("invoice");
+    console.log(invoice);
+    console.log(window);
+    var opt = {
+      margin: 1,
+      filename: "perfectCollectionTotalEstimate.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+    };
+    html2pdf().from(invoice).set(opt).save();
+    setTimeout(() => {
+      window.location.reload();
+    }, 5000);
+  });
+};
+
+const shopItems = {
+  1: "Kurta Pajama",
+  2: "Nighty",
+  3: "Lower",
+  4: "Shorts",
+  5: "T. Shirt",
+  6: "Capri",
+  7: "Under Garments",
+  8: "Night Suit",
+  9: "Towel",
+  10: "Hankey",
+  11: "Socks",
+  12: "Dhoti",
+  13: "Kurta",
+  14: "Thermal",
+  15: "Track Suit",
+};
+
+function increaseTotalItemsCount() {
+  document.getElementById("page-total-items").innerText =
+    document.querySelectorAll("#table-body >tr").length;
+}
+function updatePageTotals() {
+  //updating the page total
+  var x = document.querySelectorAll("#table-body .item-total");
+  var tot = 0;
+  for (var i = 0; i < x.length; i++) {
+    if (Number.isNaN(parseInt(x[i].innerText))) {
+      tot = 0;
+      break;
+    }
+    tot += parseInt(x[i].innerText);
+  }
+
+  if (tot >= 0) {
+    document.getElementById("page-total-sum").innerText = tot;
+    var discount = tot / 10;
+    document.getElementById("page-total-discountMinus").innerText =
+      "-" + discount;
+    document.getElementById("paget-total-netTotal").innerText = tot - discount;
+  }
+}
+function additemNumberToListOnEnter(event) {
+  console.log(event.key);
+  if (event.key === "Enter") {
+    additemNumberToList();
+  }
+}
+function additemNumberToList() {
+  var newItemNumber = document.getElementById("new-item-number").value;
+  var newItemPrice = document.getElementById("new-item-price-input").value;
+  var newItemQuantity = document.getElementById(
+    "new-item-quantity-input"
+  ).value;
+  if (
+    !(
+      newItemNumber &&
+      newItemPrice &&
+      newItemQuantity &&
+      newItemNumber > 0 &&
+      newItemPrice > 0 &&
+      newItemQuantity > 0
+    )
+  )
+    return;
+
+  document.getElementById("new-item-number").value = "";
+  document.getElementById("new-item-price-input").value = "";
+  document.getElementById("new-item-quantity-input").value = "";
+
+  var newRowTotal = newItemPrice * newItemQuantity;
+
+  //----------------------------**.   Making List Item Card
+  var unqiueId = document.querySelectorAll("#table-body >tr").length;
+  unqiueId = "id_" + unqiueId;
+  var itemName = shopItems[newItemNumber];
+  if (typeof itemName === "undefined") return;
+
+  var listItem = `<tr id=${unqiueId} class="item-row">
+   <td> <h6 class="mb-0 item-name">${itemName}</h6> </td> 
+   <td> <span class="item-price">${newItemPrice}</span> </td> 
+   <td> <span class="item-quantity">${newItemQuantity}</span> </td> 
+   <td> 
+   <span class="font-weight-semibold item-total">${newRowTotal}</span>
+    <button class="item-delete btn btn-danger" class="item-delete" style="margin-left: 1rem" onclick="handleDelete(event)" > X </button> 
+    </td> 
+    </tr>`;
+
+  document.getElementById("table-body").innerHTML += listItem;
+  // increase total items count
+  increaseTotalItemsCount();
+  updatePageTotals();
+}
+
+function handleDelete(event) {
+  var changedId = event.target.parentNode.parentNode.id;
+  console.log(changedId);
+  document.getElementById(changedId).remove();
+  increaseTotalItemsCount();
+  updatePageTotals();
+}
+
+// function priceQuantityChanged(event) {
+//   var changedId = event.target.parentNode.parentNode.id;
+//   var changedRow = document.getElementById(changedId);
+
+//   var price = changedRow.getElementsByClassName("item-price")[0].value;
+//   var quantity = changedRow.getElementsByClassName("item-quantity")[0].value;
+//   price = parseInt(price.trim());
+//   quantity = parseInt(quantity.trim());
+
+//   if (quantity && price && price > 0 && quantity > 0) {
+//     console.log(parseInt(price) + "->>" + parseInt(quantity));
+//     var rowPrice = parseInt(price) * parseInt(quantity);
+//     changedRow.getElementsByClassName("item-total")[0].innerText = rowPrice;
+//   } else {
+//     changedRow.getElementsByClassName("item-total")[0].innerText = "XX";
+//   }
+
+//   updatePageTotals();
+// }
